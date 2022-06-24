cdd3df2862c72d18e627cce381ae280e5fa91110
itsAdil45 <sheikhadil086@gmail.com>
2022-06-24 20:03:53 +0500

    basic redux implemented

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css"
+import { useDispatch, useSelector } from "react-redux";
+import { incNumber, decNumber } from "./actions";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+export default function App() {
+  const mystate = useSelector((state) => state.changeNumber);
+  const dispatch = useDispatch();
+  return <div className="Container">
+
+    <h1>React Redux Application</h1>
+    <div>
+      <a className="minus" title="minus" onClick={() => dispatch(decNumber())}>
+        <span>-</span>
+      </a>
+      <input value={mystate} className="input" />
+      <a className="plus" title="plus" onClick={() => dispatch(incNumber())}>
+        <span>+</span>
+      </a>
     </div>
-  );
-}
 
-export default App;
+
+  </div>;
+}
